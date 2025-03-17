ff61fe64b1366aec9dfac35928c0da6ec3acbb4b
Martynas Katkus <katusmartynas@gmail.com>
2025-03-18 01:43:47 +0200

    first commit boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const input = document.getElementById("input");
+const search = document.getElementById("search");
+const output = document.getElementById("outputField");
+
+search.addEventListener("click", async (e) => {
+    e.preventDefault();
+    
+})
