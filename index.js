de00d23b83c481d816b7d9c76f36af0e8e1c1c38
Martynas Katkus <katusmartynas@gmail.com>
2025-03-19 23:20:44 +0200

    paskutinis variantas

--- index.js
+++ index.js
@@ -3,8 +3,41 @@
 const input = document.getElementById("input");
 const search = document.getElementById("search");
 const output = document.getElementById("outputField");
+const api_key = "39c2bb2b";
 
 search.addEventListener("click", async (e) => {
     e.preventDefault();
-    
+    output.innerHTML = "";
+    let data;
+    try{
+        const result = await fetch(`http://www.omdbapi.com/?t=${input.value.trim()}&apikey=${api_key}&`);
+        data = await result.json();
+        console.log(data);
+    } catch(err){
+        alert("err" + err.message);
+        console.log(err);
+    }
+    const card = document.createElement("div");
+    card.classList.add("movieCard");
+
+    const img = document.createElement("img");
+    if(data["Poster"] == "N/A"){
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png?20210521171500"
+    } else {
+        img.src = data['Poster'];
+    }
+    img.classList.add("movieCardimg");
+
+    const title = document.createElement("h1");
+    title.classList.add("movieCardtitle");
+    title.textContent = data['Title'];
+
+    const date = document.createElement("p");
+    date.classList.add("movieCardrelease");
+    date.textContent = data["Released"];
+
+    card.append(img, title, date);
+    output.appendChild(card)
+
 })
+
